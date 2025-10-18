8350a26ab3cfb7ce6342cfe14ec8a6826e9946ee
Fresnel Jean Claude <fresneljeanclaudecossou64@gmail.com>
2025-10-18 15:47:46 +0100

    first-commit

---
+++ nexorax-backend/src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true, // seulement en dev
+    }),
+    UsersModule,
+  ],
+})
+export class AppModule {}
